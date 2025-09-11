9eb59c6020394e5a55ca00ebc2a0e3b1e2bd1247
Luis  Mariano <luismariano278@gmail.com>
2025-09-10 23:23:41 -0600

    tipos de datos

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,1 +1,30 @@
-document.write("<h1>hello world!</h1>");
+//tipos de datos
+"Hello world"//string
+'Hello world'//string
+
+100000 // number
+-2.3
+
+//boolean
+true 
+false
+
+
+// array 
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+// object
+
+'ryan'
+70.4
+14 true 
+
+{
+    username: 'ryan',
+    score: 70.4,
+    hours: 14,
+    professional: true
+    
+}
