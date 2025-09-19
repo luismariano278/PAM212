73c48c2ab81f0f9cfe811a2c6fce075336f1476f
Luis  Mariano <luismariano278@gmail.com>
2025-09-18 19:07:58 -0600

    ejercicio 2

--- Practica 1/app.js
+++ Practica 1/app.js
@@ -4,3 +4,10 @@ let edad = 25;
 
 let saludo = "hola, " + nombre + ". Tienes " + edad + " años.";
 console.log(saludo);
+
+//Ejercicio 2
+ console.log("ejercicio 2")
+const cuadradoo = numero => numero * numero; // funcion flecha
+console.log(cuadradoo(5));
+console.log(cuadradoo(6));
+console.log(cuadradoo(7));
