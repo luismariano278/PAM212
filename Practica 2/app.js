1e9c586d1666b64de94f41ac207fac59989e55fc
Luis  Mariano <luismariano278@gmail.com>
2025-09-19 18:57:24 -0600

    ejercicio 2

--- Practica 2/app.js
+++ Practica 2/app.js
@@ -1,4 +1,4 @@
-//ejercicio 1
+//ejercicio 1 
 const persona = {
     nombre: "Ivan Isay",
     edad: 37,
@@ -10,3 +10,17 @@ const persona = {
 
 const {nombre, edad, direccion:{ciudad, pais}} = persona;
 console.log("me llamo " + nombre + " tengo " + edad + " y vivo en " + ciudad);
+
+
+//ejercicio 2
+const productos = [
+    {nombres: "Laptop", precio: 12000},
+    {nombres: "Mouse", precio: 250},
+    {nombres: "Teclado", precio: 750},
+    {nombres: "Monitor", precio: 3000},
+];
+
+const nombres = productos
+    .filter(productos => productos.precio > 1000 )
+    .map(productos => productos.nombres);
+console.log(nombres);
