55004b55987921279d3d90bb1f06027bce4f9cef
Luis  Mariano <luismariano278@gmail.com>
2025-09-19 19:42:05 -0600

    ejercicio 3

--- Practica 2/app.js
+++ Practica 2/app.js
@@ -1,4 +1,5 @@
 //ejercicio 1 
+/* 
 const persona = {
     nombre: "Ivan Isay",
     edad: 37,
@@ -10,7 +11,7 @@ const persona = {
 
 const {nombre, edad, direccion:{ciudad, pais}} = persona;
 console.log("me llamo " + nombre + " tengo " + edad + " y vivo en " + ciudad);
-
+*/
 
 //ejercicio 2
 const productos = [
@@ -24,3 +25,28 @@ const nombres = productos
     .filter(productos => productos.precio > 1000 )
     .map(productos => productos.nombres);
 console.log(nombres);
+
+//ejercicio 3 
+ const personas = [
+    {nombre: "Ana", edad: 22},
+    {nombre: "Luis", edad: 35},
+    {nombre: "María", edad: 28}
+ ];
+
+ // a)
+
+ const nombre = personas
+ .find(persona => persona.nombre === "Luis")
+ console.log(nombre)
+
+ // b)
+personas.forEach(persona => 
+{
+console.log(persona.nombre + " tiene " + persona.edad)
+});
+
+// c)
+SumaEdades = personas.reduce((acumulador,persona) => acumulador + persona.edad, 0)
+console.log("La suma total de las edades es: " + SumaEdades);
+
+
