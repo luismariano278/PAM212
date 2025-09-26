71b764e99ef46c45abca87645d7765bc67e8ddfd
Luis  Mariano <luismariano278@gmail.com>
2025-09-25 18:01:21 -0600

    ejercicio1 (con pruebas)

--- Practica 3/main.js
+++ Practica 3/main.js
@@ -1,3 +1,9 @@
 import {restar} from './utils.js';
 
 console.log(restar(9,5));
+console.log(restar(8,5));
+console.log(restar(11,5));
+console.log(restar(9,6));
+console.log(restar(12,555));
+console.log(restar(40000,569));
+console.log(restar(91234,55649));
