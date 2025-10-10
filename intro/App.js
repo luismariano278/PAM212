c837e63e0169674f9f304faaf829dad9bbc8f119
Luis  Mariano <luismariano278@gmail.com>
2025-10-10 17:20:37 -0600

    Update App.js

--- intro/App.js
+++ intro/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React,{useState} from 'react';
 
 export default function App() {
