33ad5b3243b4db93d2ce19cecd52e6f58dd1a2c2
Luis  Mariano <luismariano278@gmail.com>
2025-10-10 16:29:22 -0600

    Practica 5
    I

--- intro/App.js
+++ intro/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
 
 export default function App() {
+  const[contador,setContador] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo react native :D!</Text>
+      <Text>Contador: {contador} </Text>
+      <Button title="Incrementar" onPress={() => setContador(contador + 1)}/>
+      <Button title="Quitar" onPress={() => setContador(contador - 1)}/>
+      <Button title="Reiniciar" onPress={() => setContador(0)}/>
       <StatusBar style="auto" />
     </View>
   );
