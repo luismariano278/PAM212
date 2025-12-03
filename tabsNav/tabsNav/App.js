1581e452d47ca944dd6f247bf246d2c100f7a4f3
Luis  Mariano <luismariano278@gmail.com>
2025-12-03 17:56:43 -0600

    Practica No.18: Tabs & Stack
    tuve que crear otro proyecto pq el otro tronó

---
+++ tabsNav/tabsNav/App.js
@@ -0,0 +1,47 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
+
+import Home from './screens/home';
+import Profile from './screens/profile';
+import Settings from './screens/settings';
+import Detalle from './screens/detalle';  
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackEnDetalle() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Detalle" component={Detalle} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') iconName = 'home-outline';
+            if (route.name === 'ProfileStack') iconName = 'person-outline';
+            if (route.name === 'Settings') iconName = 'settings-outline';
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="ProfileStack" component={StackEnDetalle} options={{ title: "Profile", headerShown: false }} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ tabsNav/tabsNav/screens/detalle.js
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet, } from "react-native";
+
+export default function Detalle() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.iconRow}>
+                <Text style={styles.title}>Perfil usuario</Text>
+                <Text style={styles.subtitle}>Detalles de usuario</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconRow: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 27,
+    color: '#000000ff',
+  },
+  subtitle: {
+    fontSize: 17,
+    color: '#1932b1ff',
+  },
+});
